b897bfd81b249816909e858b92f0d7c167abfa18
Raduionutz <radu_mocanu2000@yahoo.com>
2021-01-12 14:49:00 +0200

    Sdk 102 add ruby test driver (#1)
    feat: ruby test driver added, plus some python fixes

--- python/ionos-cloud-test-driver-sdk-python.py
+++ python/ionos-cloud-test-driver-sdk-python.py
@@ -1,89 +1,81 @@
-#!/usr/bin/env python3
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 import sys
 import json
-from . import api as request_api_package
-from .exceptions import ApiException
 import ionoscloud
 import re
 import os
 
-configuration = ionoscloud.Configuration(
-    username=os.environ.get('IONOS_USERNAME'),
-    password=os.environ.get('IONOS_PASSWORD')
-)
-api_client = ionoscloud.ApiClient(configuration)
-
-input = sys.stdin.readlines()
-testing_data = json.loads(input[0])
-
-operation = testing_data['operation']
-params = {re.sub(r'(?<!^)(?=[A-Z])', '_', p['name']).lower(): p['value'] for p in testing_data['params']}
 
 def get_request_classes():
     classes = []
-    for key in dir(request_api_package):
-        structure = getattr(request_api_package, key)
+    for key in dir(ionoscloud.api):
+        structure = getattr(ionoscloud.api, key)
         if isinstance(structure, type):
             classes.append(structure)
     return classes
 
+
 def get_class_and_method(operation):
     classes = get_request_classes()
+    searched_method = '%s_with_http_info' % re.sub(r'(?<!^)(?=[A-Z])', '_', operation).lower()
     for request_class in classes:
-        method_list = {func:getattr(request_class, func) for func in dir(request_class) if callable(getattr(request_class, func))}
+        method_list = {func: getattr(request_class, func) for func in dir(request_class) if callable(getattr(request_class, func))}
         for method_name, method in method_list.items():
             # camelcase to underscore for python standard
-            if method_name == '%s_with_http_info' % re.sub(r'(?<!^)(?=[A-Z])', '_', operation).lower():
+            if method_name == searched_method:
                 return request_class, method
     return None, None
 
-if operation == 'waitForRequest':
-    request_id = re.search('/requests/([-A-Fa-f0-9]+)/', params['request']).group(1)
-    # ionoscloud.RequestApi(api_client).requests_status_get,
-    api_client.wait_for_completion(request_id)
-    sys.stdout.write(json.dumps({}))
-    exit()
 
-classApi, method = get_class_and_method(operation)
-api_instance = classApi(api_client)
-try:
-    response = method(api_instance, response_type='object', **params)
-
-    return_data = response[0]
-    status_code = response[1]
-    response_headers = dict(map(lambda x: (x[0], x[1].split(",")), dict(response[2]).items()))
-
-    driver_output = {
-      "result": return_data,
-      "httpResponse": {
-        "statusCode": status_code,
-        "headers": response_headers,
-        "body": return_data
-      }
-    }
-
-    sys.stdout.write(
-        json.dumps(driver_output)
+if __name__ == "__main__":
+    configuration = ionoscloud.Configuration(
+        username=os.environ.get('IONOS_USERNAME'),
+        password=os.environ.get('IONOS_PASSWORD'),
     )
-except ApiException as e:
-    exception_body = e.body
-    exception_status = e.status
-    exception_headers = e.headers
-    sys.stdout.write(
-        json.dumps(
-            {
-                "result": 'ApiException occured',
+    api_client = ionoscloud.ApiClient(configuration)
+
+    input = sys.stdin.readlines()
+    testing_data = json.loads(input[0])
+
+    operation = testing_data['operation']
+    params = {re.sub(r'(?<!^)(?=[A-Z])', '_', p['name']).lower(): p['value'] for p in testing_data['params']}
+
+    try:
+        if operation == 'waitForRequest':
+            request_id = re.search('/requests/([-A-Fa-f0-9]+)/', params['request']).group(1)
+            api_client.wait_for_completion(request_id)
+            sys.stdout.write(json.dumps({}))
+        else:
+            classApi, method = get_class_and_method(operation)
+            return_data, status_code, response_headers = method(classApi(api_client), response_type='object', **params)
+            response_headers = dict(map(lambda x: (x[0], x[1].split(",")), dict(response_headers).items()))
+
+            sys.stdout.write(json.dumps({
+                "result": return_data,
                 "httpResponse": {
-                    "statusCode": exception_status,
-                    "headers": dict(exception_headers),
-                    "body": exception_body
-                },
-                "error": exception_body
-            }
-        )
-    )
-except Exception as e:
-    sys.stdout.write("General Exception occured: ")
-    sys.stdout.write(e)
+                    "statusCode": status_code,
+                    "headers": response_headers,
+                    "body": return_data,
+                }
+            }))
+    except ionoscloud.exceptions.ApiException as e:
+        sys.stdout.write(json.dumps({
+            "result": 'ApiException occured',
+            "httpResponse": {
+                "statusCode": e.status,
+                "headers": dict(e.headers),
+                "body": e.body,
+            },
+            "error": e.body,
+        }))
+    except Exception as e:
+        sys.stdout.write(json.dumps({
+            "result": 'GeneralException occured',
+            "httpResponse": {
+                "statusCode": None,
+                "headers": None,
+                "body": str(e),
+            },
+            "error": str(e),
+        }))

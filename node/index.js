ade060c0305747495b415e3ed830cdc9bc27a490
Florin Mihalache <florin.mihalache@ionos.com>
2021-05-26 17:45:11 +0300

    testing

--- node/index.js
+++ node/index.js
@@ -15,21 +15,23 @@ if (password === undefined) {
 }
 
 
-try {
-  const config = new sdk.Configuration({username, password});
+(async function () {
+  try {
+    const config = new sdk.Configuration({username, password});
 
-  config.setDepth(10).setPretty(true);
+    config.setDepth(10).setPretty(true);
 
-  const {operation, params} = await payload.parse()
-  const response = await api.find(operation).run(config, params)
-  output.success(response)
+    const {operation, params} = await payload.parse()
+    const response = await api.find(operation).run(config, params)
+    output.success(response)
 
-} catch(error) {
-  if (error.response === undefined) {
-    output.error(error.message, error.stack)
-  } else {
-    output.apiError(error.response)
+  } catch (error) {
+    if (error.response === undefined) {
+      output.error(error.message, error.stack)
+    } else {
+      output.apiError(error.response)
+    }
   }
-}
+})()
 
 

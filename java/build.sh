642d11adb2fdf2cf2f5d7bed18ab4b7c9a53651b
cristi <cristian.mihai.guran@ionos.com>
2022-08-24 17:12:08 +0300

    feat: allow multiple package names for java

---
+++ java/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# {App} - used to replace com.ionoscloud with the provided package name(eg. com.ionoscloud.dbaaspostgres)
+#
+
+function usage() {
+	echo "error: $1"
+	echo
+	echo "usage: build.sh <core_lib_path>"
+	echo "example: build.sh com.ionoscloud.dbaaspostgres"
+	exit 1
+}
+
+
+core_lib_path="${1}"
+if [ "${core_lib_path}" = "" ]; then
+	usage "path to core lib not specified"
+fi
+
+# replace core lib version
+# NOTE: THIS RUNS ON LINUX ONLY - DOESN'T WORK ON OSX - on OSX the -i flag requires an extension name to save backups to
+ find . -name "*.java" -exec sed -i "s/com.ionoscloud/${core_lib_path}/g" {} \; || exit 1

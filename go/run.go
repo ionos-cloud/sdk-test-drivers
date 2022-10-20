4e4e7547a1b0acf5d83d89ee73d661965f8a6a3f
cristi <cristian.mihai.guran@ionos.com>
2022-10-20 13:15:56 +0300

    fix: return ionostime

--- go/run.go
+++ go/run.go
@@ -66,22 +66,25 @@ func getDecoder(result interface{}) (*mapstructure.Decoder, error) {
 }
 
 // ToTimeHookFunc - decodes ionosTime before it is parsed
-func ToTimeHookFunc() mapstructure.DecodeHookFunc {
+func ToTimeHookFunc() mapstructure.DecodeHookFuncType {
 	return func(
 		f reflect.Type,
 		t reflect.Type,
 		data interface{}) (interface{}, error) {
-		if t != reflect.TypeOf(sdk.IonosTime{}) {
+		if t != reflect.TypeOf(&sdk.IonosTime{}) && t != reflect.TypeOf(sdk.IonosTime{}) {
 			return data, nil
 		}
 
 		switch f.Kind() {
 		case reflect.String:
-			return time.Parse(time.RFC3339, data.(string))
+			valTime, _ := time.Parse(time.RFC3339, data.(string))
+			return sdk.IonosTime{Time: valTime}, nil
 		case reflect.Float64:
-			return time.Unix(0, int64(data.(float64))*int64(time.Millisecond)), nil
+			valTime := time.Unix(0, int64(data.(float64))*int64(time.Millisecond))
+			return sdk.IonosTime{Time: valTime}, nil
 		case reflect.Int64:
-			return time.Unix(0, data.(int64)*int64(time.Millisecond)), nil
+			valTime := time.Unix(0, data.(int64)*int64(time.Millisecond))
+			return sdk.IonosTime{Time: valTime}, nil
 		default:
 			return data, nil
 		}
